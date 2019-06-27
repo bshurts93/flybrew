3227c5d2be3b2365b8ecf043102bcd0d818fc3b7
Ben Shurts <bshurts93@gmail.com>
2019-06-27 16:49:05 -0500

    Cards on the places page now link to a country page to be rendered. Whichever country is selected then has it's title and description displayed on the main card

--- flybrew/client/src/components/views/country.js
+++ flybrew/client/src/components/views/country.js
@@ -14,47 +14,45 @@ const currentPage = () => {
 const countries = [
   {
     name: "Germany",
-    body: "Water, hops, barley and yeast. No more, no less.",
-    image: "https://flaglane.com/download/german-flag/german-flag-medium.jpg"
+    body:
+      "Willkommen aus Deutchland! The land of beer, brats and bretzels. When it comes to beer around the world, Germany is known for some of the purest, finest brews ever made. A traditional brewing culture holds the brewing process to a high standard, and the results speak for themselves."
   },
   {
     name: "Belgium",
-    body: "Rich Quads, Spicy Saisons and Tart Lambics.",
-    image: "https://flaglane.com/download/belgian-flag/belgian-flag-medium.jpg"
+    body:
+      "Belgian brewers often supplement the balance of malt sweetness and hop bitterness you find in every beer with fruity, spicy, floral and herbal aromas. Some Belgian beers are like running through a hayfield at harvest and others explode with a whole orchard of fruit flavours. You can sometimes find earthiness, a savoury, mushroomy umami taste, and you can sometimes find sourness, ranging from a light tartness to a mouth-puckering acidity."
   },
   {
     name: "Czech Republic",
-    body: "Not just Pilsners.",
-    image: "https://flaglane.com/download/czech-flag/czech-flag-medium.jpg"
+    body:
+      "Let's take it back. Like way back. Try over 1000 years back. Brewing has been a part of Czech history since the year 993, when beer was almost exclusively produced at monasteries. Bohemian brewing practices became world renowned by the mid-19th century when the citizens and brewers of Plzen created the now hugely popular style known as the Pilsner. Today, citizens of the Czech Republic proudly hold the title of highest annual beer consumption per capita at 142.4 liters. To that, we say Na Zdravi!"
   },
   {
     name: "Austria",
-    body: "Vienna to Salzburg, the Lager is king",
-    image:
-      "https://flaglane.com/download/austrian-flag/austrian-flag-medium.jpg"
+    body:
+      "The hills are alive with the sound of good beer. And lots of it, considering that Austria holds a close second position behind the Czech Republic for the highest annual beer consumption per capita, at 106	liters per person. The Viennese Lager made famous in the 1840s remains one of the highest regarded styles in Europe, but make sure to work your way through the full spectrum of tried and true styles from across the country."
   },
   {
     name: "Norway",
-    body: "Where there's a farmhouse, there's a brewhouse.",
-    image:
-      "https://flaglane.com/download/norwegian-flag/norwegian-flag-medium.jpg"
+    body:
+      "With a landscape so beautiful, there must be pints to match, right? Well lucky for us, Norway has over a millenium of brewing expertise in the hands of the farmers who frequently had a brew house on site. These farmers would use their own strains of yeast in the 'bryggehus' (farm-brew house) to create some one of a kind beers over the years. There are even historic Norwegian strains of yeast (Kveik) that have made a splash in the US craft brewing scene!"
   },
   {
     name: "Ireland",
-    body: "Reds to Stouts to Wee Heavies. Sláinte!",
-    image: "https://flaglane.com/download/irish-flag/irish-flag-medium.jpg"
+    body:
+      "Of course everyone who comes to Dublin has a pint of Guinness. It's a pilgrimage. But don't mistake this lovely land for a one trick pony. You can enjoy an array of traditional Irish stouts and red ales, or get adventurous and seek out the plethora of new craft breweries that have set up shop in the major cities across the country."
   }
 ];
 
 const object = arr => {
+  let match = {};
   arr.forEach(element => {
     if (element.name === currentPage()) {
-      console.log("MATCH: ", element);
+      match = element;
     }
   });
+  return match;
 };
-
-object(countries);
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -65,8 +63,7 @@ const useStyles = makeStyles(theme => ({
 
 function Country() {
   const classes = useStyles();
-  object(countries);
-  console.log(currentPage());
+  const country = object(countries);
 
   return (
     <div>
@@ -75,18 +72,14 @@ function Country() {
           <Grid item xs={8}>
             <Paper className={classes.root}>
               <Typography variant="h4" component="h3">
-                Overview -
+                Overview - {country.name}
               </Typography>
               <Typography variant="body1" component="p">
-                Willkommen aus Deutchland! The land of beer, brats and bretzels.
-                When it comes to beer around the world, Germany is known for
-                some of the purest, finest brews ever made. A traditional
-                brewing culture holds the brewing process to a high standard,
-                and the results speak for themselves.
+                {country.body}
               </Typography>
             </Paper>
             <Paper className={classes.root}>
-              <Typography variant="h4" component="h3" onClick={() => object()}>
+              <Typography variant="h4" component="h3">
                 Phrasebook
               </Typography>
               <Table />
